61a04c9ace0f3b309586d4025d42da302093b33a
RamilBinnatov <ramilrb@code.edu.az>
2022-07-13 02:01:34 +0400

    CheckWords

---
+++ Predicate/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> stringlist = new List<string>();
+            stringlist.Add("Ramil");
+            stringlist.Add("Sadiq");
+            stringlist.Add("Cavid");
+            stringlist.Add("Elnur");
+            stringlist.Add("Faxri");
+            stringlist.Add("Elvin");
+            stringlist.Add("Fariz");
+
+
+
+
+            var result = stringlist.FindAll(m => m != "Cavid");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static bool CheckWord(string word)
+        {
+            return word != "Cavid";
+        }
+
+        public static string CheckWords(Predicate<string> predicate, string[] words)
+        {
+            var name = "Cavid";
+            foreach (var item in words)
+            {
+                if (predicate(item))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            return name;
+        }
+        
+    }  
+}
